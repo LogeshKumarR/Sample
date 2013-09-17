8e7d2dd84f8370dec7f9f4e6dea1d3781eb4f048
LogeshKumarR <logeshkumarlk1994@gmail.com>
2013-09-17 15:15:51 +0530

    Mysampleadded

---
+++ Mysample/src/Samplepkg/Printhello.java
@@ -0,0 +1,14 @@
+package Samplepkg;
+
+public class Printhello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hai");
+
+	}
+
+}
